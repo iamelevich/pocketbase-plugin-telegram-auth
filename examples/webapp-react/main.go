d78b1f9cfa4902c2bdaeaf7a042c739cbbfe5dde
Ilya Amelevich <ilya.amelevich@gmail.com>
2023-02-06 15:00:32 +0100

    Add react example

---
+++ examples/webapp-react/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	tgAuthPlugin "github.com/iamelevich/pocketbase-plugin-telegram-auth"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
+	"log"
+	"os"
+
+	"github.com/pocketbase/pocketbase"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	// Setup tg auth for users collection
+	tgAuthPlugin.MustRegister(app, &tgAuthPlugin.Options{
+		BotToken:      "6086153355:AAHQR0dRvmboEIfbsPT93GHczOnT355gmUE", // Better to use ENV variable for that
+		CollectionKey: "users",
+	})
+
+	// Setup serving bundled react app
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		// serves static files from the provided public dir (if exists)
+		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./tg_webapp/dist"), true))
+		return nil
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
