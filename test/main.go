34abdd1e5af245c38d10bcc02e060dcd2240e579
Ilya Amelevich <ilya.amelevich@gmail.com>
2023-04-02 21:24:10 +0200

    test: add test server

---
+++ test/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	"github.com/pocketbase/pocketbase"
+)
+
+func main() {
+	app := pocketbase.NewWithConfig(&pocketbase.Config{
+		DefaultDataDir: "./test_pb_data",
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
